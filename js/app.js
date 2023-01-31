e39d43aee0c61a3e6b8a7d249ab19e16893033a4
Shane Hendrickson <shane.hendrickson@gmail.com>
2023-01-30 19:30:24 -0500

    Final vanilla commit
    Revisit this to add my own flair

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const slider = document.getElementById("slider");
+const sliderFill = document.querySelector(".fill");
 const timeLeftText = document.getElementById("time-left")
 const startCount = 5
 let timeLeft = startCount
@@ -6,9 +6,33 @@ let timeLeft = startCount
 const timerId = setInterval(() => {
   timeLeft--
   timeLeftText.textContent = timeLeft;
-  slider.value = timeLeft
+  sliderFill.style.width = (timeLeft / startCount) * 100 + "%"
   if (timeLeft <= 0){
     clearInterval(timerId);
-    timeLeftText.textContent = "TIME OVER"
+    timeLeftText.textContent = "END"
+    surprise()
   }
 }, 1000)
+
+function surprise(){
+  const colors = [
+    "rgba(255, 205, 97, 0.5)", 
+    "rgba(97, 168, 255, 0.5)",
+    "rgba(247, 255, 97, 0.5)",
+    "rgba(97, 255, 184, 0.5)",
+    "rgba(255, 255, 255, 0.5)"
+  ]
+
+  for(let i = 0; i < 500; i++){
+    setTimeout(() => {
+      const circleElement = document.createElement("div");
+      circleElement.classList.add("circle");
+      circleElement.style.left = Math.floor(Math.random() * 100) + "%";
+      circleElement.style.top = Math.floor(Math.random() * 100) + "%";
+      circleElement.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+      timeLeftText.append(circleElement);
+    }, i * 5)
+  }
+  
+
+}
